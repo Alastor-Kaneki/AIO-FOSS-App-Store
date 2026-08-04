0d872ca8733922c8f7f415c86556feb10b73f854
Alastor Kaneki <170276077+Alastor-Kaneki@users.noreply.github.com>
2026-08-04 07:12:38 -0400

    Raise minimum Android version for Shizuku 13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "dev.alastorkaneki.gitdroid"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 36
         versionCode = 2
         versionName = "0.2.0"
